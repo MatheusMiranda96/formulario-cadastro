032d3242dd4382547a693ccd9b127015d484d0ad
MatheusMiranda96 <matth.miranda@gmail.com>
2022-03-24 13:51:35 -0300

    vailidacao de caracteres

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ class Validator {
     
     constructor(){
         this.validations = [
-
+            'data-min-length',
         ]
     }
 
@@ -12,7 +12,7 @@ class Validator {
         //pegar os inputs
         let inputs = form.getElementsByTagName('input');
 
-        console.log(inputs);
+        //console.log(inputs);
 
         // transformo uma HTMLCollection -> array
         let inputsArray = [...inputs];
@@ -20,7 +20,45 @@ class Validator {
         // loop nos inputs e validação mediante aos atributos encontrados
         inputsArray.forEach(function(input){
 
-        });
+            // fazer validação de acordo com o atributo do input
+            for(let i = 0; this.validations.length > i; i++){
+                if (input.getAttribute(this.validations[i]) != null){
+                    
+                    // limpa string para saber o método
+                    let method = this.validations[i].replace("data-", "").replace("-", "");
+                    
+                    // valor do input
+                    let value = input.getAttribute(this.validations[i]);
+
+                    // invoca o método
+                    this[method](input,value);
+
+                }
+            }
+        }, this);
+    }
+    // método para validar se tem um mínimo de caracteres
+    minlength(input, minValue) {
+        
+        let inputLength = input.value.length;
+
+        let errorMessage = `O campo precisa ter pelo menos ${minValue} caracteres`;
+
+        if(inputLength < minValue) {
+            this.printMessage(input, errorMessage);
+          }
+    }
+
+    printMessage(input, msg) {
+        let template = document.querySelector('.error-validation').cloneNode(true);
+
+        template.textContent = msg;
+        
+        let inputParent = input.parentNode;
+
+        template.classList.remove('template');
+
+        inputParent.appendChild(template);
     }
 }
 
